168d1a7cc0e3cfed59de60412dfce8b9d92317f7
Patrick Louis <patricklouisw@gmail.com>
2024-12-18 18:01:52 -0500

    Static Resource with LWC

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
+import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets'
 
-export default class AlarmClockApp extends LightningElement {}
+export default class AlarmClockApp extends LightningElement {
+    clockImage = AlarmClockAssets + '/AlarmClockAssets/clock.png'
+}
